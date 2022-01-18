59afd86701141470905e695d03b339a2d2575b2d
FRC4573 <admin@rambotics4573.club>
2022-01-18 18:38:57 -0500

    bruh

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -55,7 +55,7 @@ public class Robot extends TimedRobot {
     */                    
   // Drive Stick \\
   //////////////////////////BUTTON MAPPINGs\\\\\\\\\\\\\\\\\\\\\\\\\
-  static boolean commandRan = false;
+  static boolean commandRan = true;
 
     //////////////////////////Motor Controllers\\\\\\\\\\\\\\\\\\\\\\\\\
   WPI_VictorSPX m_frontRight = new WPI_VictorSPX(1);
