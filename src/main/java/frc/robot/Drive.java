0d6f033017a7c5478b4eff39011617866dcafd59
FRC4573 <admin@rambotics4573.club>
2022-01-24 19:02:31 -0500

    shooter added

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -1,7 +1,11 @@
 package frc.robot;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+//Niko was here
 
 public class Drive {
   private WPI_VictorSPX m_frontRight = new WPI_VictorSPX(1);
@@ -11,14 +15,23 @@ public class Drive {
   
   private SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
   private SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+  private DifferentialDrive m_drive = new DifferentialDrive(this.m_left, this.m_right);
 
   public Drive(){
-      
+    m_drive.setExpiration(0.50);
+    m_drive.arcadeDrive(0, 0, true);
+    m_drive.setSafetyEnabled(false);
   }
-public SpeedControllerGroup getLeftSpeedController(){
+
+public DifferentialDrive getDriveTrain(){
+  return this.m_drive;
+}
+
+  public SpeedControllerGroup getLeftSpeedController(){
     return this.m_left;
-}
-public SpeedControllerGroup getRightSpeedController(){
+  }
+
+  public SpeedControllerGroup getRightSpeedController(){
     return this.m_right;
-}
+  }
 }
---
+++ src/main/java/frc/robot/Shooter.java
@@ -0,0 +1,53 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Talon;
+
+//Niko was here
+
+public class Shooter {
+    private Talon motor1 = new Talon(0);
+    private Talon motor2 = new Talon(1);
+    private Talon motor3 = new Talon(2);
+
+    //Forward Functions
+    public Talon getMotor1(){
+        return this.motor1;
+    }
+    public Talon getMotor2(){
+        return this.motor2;
+    }
+    public Talon getMotor3(){
+        return this.motor3;
+    }
+    public void setMotor1Forward(){
+        this.motor1.setSpeed(0.8);
+    }
+    public void setMotor2Forward(){
+        this.motor2.setSpeed(0.8);
+    }
+    public void setMotor3Forward(){
+        this.motor3.setSpeed(0.8);
+    }
+
+    //Reverse Functions
+    public void setMotorReverse1(){
+        this.motor1.setSpeed(-1);
+    }
+    public void setMotorReverse2(){
+        this.motor2.setSpeed(-1);
+    }   
+    public void setMotorReverse3(){
+        this.motor3.setSpeed(-0.5);
+    }
+
+    //Stopping Functions
+    public void setMotorStop1(){
+        this.motor1.setSpeed(0.0);
+    }
+    public void setMotorStop2(){
+        this.motor2.setSpeed(0.0);
+    }
+    public void setMotorStop3(){
+        this.motor3.setSpeed(0.0);
+    }
+}
